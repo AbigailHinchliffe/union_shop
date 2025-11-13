2752ccda08701099a86fc43d96fa94a055ffbfa9
AbigailHinchliffe <abigail.hinchliffe@gmail.com>
2025-11-13 19:41:35 +0000

    Changed header text

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/product_page.dart';
 import 'package:union_shop/widgets/header.dart';
+//import 'package:union_shop/widgets/about_us.dart';
+
 
 void main() {
   runApp(const UnionShopApp());
