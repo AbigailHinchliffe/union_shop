9522ccde4976eff343ae1e4f3eb181cfaf887ff6
AbigailHinchliffe <abigail.hinchliffe@gmail.com>
2025-11-29 08:19:40 +0000

    Added appshell formatting to about us page

--- lib/about_us.dart
+++ lib/about_us.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/widgets/appshell.dart';
 
 class AboutUs extends StatelessWidget {
   const AboutUs({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('About The Union')
+    return Appshell(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Text(
+              'We are the University of Portsmouth Students Union, est. since 1911, we represent over 23,000 students at the University of Portsmouth. Our mission is to...',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          ),
+        ),
       ),
-      body: const Center(
-        child: Text('We are the University of Portsmouth Students Union, est. since 1911, we represent over 23,000 students at the University of Portsmouth. Our mission is to...')
-      )
     );
   }
 }
