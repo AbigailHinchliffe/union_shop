27b47bf123c95878efcf75905fd8da23418800a3
AbigailHinchliffe <abigail.hinchliffe@gmail.com>
2025-11-29 09:56:37 +0000

    Changed text formatting for about us page

--- lib/about_us.dart
+++ lib/about_us.dart
@@ -10,12 +10,22 @@ class AboutUs extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Center(
-            child: Text(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+            const Text('About Us',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20,
+            fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height:12),
+            Center(
+              child:Text(
               'We are the University of Portsmouth Students Union, est. since 1911, we represent over 23,000 students at the University of Portsmouth. Our mission is to...',
               textAlign: TextAlign.center,
               style: Theme.of(context).textTheme.bodyMedium,
-            ),
+            ),),
+          ],
           ),
         ),
       ),
