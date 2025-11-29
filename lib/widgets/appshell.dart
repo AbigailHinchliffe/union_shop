b8d32d05cfacb1ab11e1b98d9d9790de3669f027
AbigailHinchliffe <abigail.hinchliffe@gmail.com>
2025-11-29 08:11:08 +0000

    Added general shell layout for all pages

---
+++ lib/widgets/appshell.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:union_shop/widgets/header.dart';
+import 'package:union_shop/widgets/footer.dart';
+
+class Appshell extends StatelessWidget{
+  final Widget body;
+  const Appshell({super.key, required this.body});
+
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      body: Column( 
+        children: [
+          const AppHeader(),
+          Expanded(child:body),
+          const Footer(),
+          ],
+        ),
+      );
+  }
+}
